57414dd2b7f7092ccde1a9618d1fcaf5f0143405
MDLicht <zombie612@naver.com>
2025-03-09 01:51:32 +0900

    버전 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mdlicht.zb.composesampleapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
